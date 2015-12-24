6765e525093b8547ad06889468cc8bbae5ac6822
Rinze Bruining <rbruining@alten.nl>
2015-12-24 11:16:33 +0100

    Use apiManager to update heatmaps, which refreshes layer automatically when an update is received.

--- server.ts
+++ server.ts
@@ -1,13 +1,36 @@
 import Winston = require('winston');
 import * as csweb from "csweb";
 import fs = require('fs');
+import path = require('path');
 
 Winston.remove(Winston.transports.Console);
 Winston.add(Winston.transports.Console, <Winston.ConsoleTransportOptions>{
     colorize: true,
     label: 'csWeb',
     prettyPrint: true
-})
+});
+
+var apiLayersFolder = path.join(__dirname, 'public', 'data', 'api', 'layers');
+
+// Clean old heatmap layers before starting the server
+fs.readdir(apiLayersFolder, (err, files) => {
+    if (err) {
+        console.log(err);
+    } else {
+        files.forEach((f) => {
+            fs.unlink(path.join(apiLayersFolder, f), (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Deleted ' + f);
+                }
+            });
+        });
+    }
+});
+// Create empty heatmap layer
+var layer = createGridLayer('');
+fs.writeFile(path.join(apiLayersFolder, 'hmfheatmap.json'), JSON.stringify(layer), (err) => { });
 
 var cs = new csweb.csServer(__dirname, <csweb.csServerOptions>{
     port: 3003
@@ -17,11 +40,9 @@ cs.start(() => {
     cs.server.post('/api/hmf', (req, res) => {
         if (req.body.hasOwnProperty('grid')) {
             var grid = req.body.grid;
-            fs.writeFile(__dirname + '/public/data/projects/hmf/hmfheatmap.asc', grid, (err) => {
-                if (err) {
-                    console.log('Error writing heatmap: ' + err);
-                }
-                console.log('Heatmap saved successfully!');
+            var layer = createGridLayer(grid);
+            cs.api.addUpdateLayer(layer, {}, () => {
+                console.log('Heatmap saved');
             });
         } else {
             console.log('No grid data found');
@@ -29,3 +50,37 @@ cs.start(() => {
         res.end();
     });
 });
+
+function createGridLayer(grid: string) {
+    var layer: csweb.ILayer = {
+        id: 'hmfheatmap',
+        title: 'Heatmap',
+        description: 'Overlay showing probable locations of finding the missing child',
+        features: [],
+        storage: 'file',
+        enabled: true,
+        isDynamic: true,
+        data: grid,
+        url: 'api/layers/hmfheatmap',
+        typeUrl: `data/resourcetypes/hmfheatmaptypes.json`,
+        type: 'grid',
+        renderType: 'gridlayer',
+        dataSourceParameters: <csweb.IGridDataSourceParameters>{
+            propertyName: 'h',
+            gridType: 'esri',
+            projection: 'WGS84',
+            contourLevels: [
+                0,
+                20,
+                40,
+                60,
+                80,
+                100
+            ],
+            skipLines: 5
+        },
+        defaultFeatureType: 'heat',
+        defaultLegendProperty: 'h'
+    };
+    return layer;
+}
